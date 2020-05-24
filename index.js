857d46c785ce8eacfd3683a823d99b925bd08177
Shahad Puthukudi <shahadp@gmail.com>
2020-05-24 00:22:22 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,37 @@
 function theBeatlesPlay(musicians, instruments)
 {
   let empty=[];
-  for (int i=0;i<musicians.length();i++)
+  for (let i=0;i<musicians.length;i++)
+  {
   empty.push(`${musicians[i]} plays ${instruments[i]}`);
-  retrun empty;
+  console.log(empty);
+  }
+  return empty;
+}
+
+
+function johnLennonFacts(facts)
+{
+  let i=0;
+  let factsCopy=[];
+  console.log(`<====>input ${facts} size ${facts.length}`)
+  while (i<facts.length)
+  {
+    factsCopy.push(facts[i]+'!!!');
+    console.log(i);
+    i++;
+  }
+  console.log(factsCopy);
+  return factsCopy;
+}
+
+
+function iLoveTheBeatles(n)
+{
+  let empty=[];
+  do{
+    empty.push('I love the Beatles!')
+    n++;
+  }while(n<15)
+  return empty;
 }
